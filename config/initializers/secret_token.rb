d37121e37ac921635c15e78812a0e3086e77e9c7
Konstantinos Patikas <patikas@ceid.upatras.gr>
2014-09-11 00:42:39 +0300

    added welcome controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyApp::Application.config.secret_token = '2ebcfeff152bc12dfd11a5699731ce2dcf40747be32ec6b20db648145361be17a6188e3b2f40100bdede73fc0d5a1535f6d946b802321562d2e581f38901ca49'
